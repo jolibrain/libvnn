5d759b6bf13abb614a02a72186e5533d1535d87c
Nicolas Bertrand <nicolas@indecp.org>
2018-06-18 18:29:38 +0200

    libvnn: create cpp skeletons for src
    - based on C++ policy-based approch
    - compilation with cmake

---
+++ src/inputconnectorstrategy.h
@@ -0,0 +1,38 @@
+/**
+ * libvnn
+ * Copyright (c) 2018 Nicolas Bertrand
+ * Author:
+ *
+ * This file is part of libvnn.
+ *
+ * deepdetect is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * deepdetect is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with deepdetect.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INPUTCONNECTORSTRATEGY_H
+#define INPUTCONNACTORSTRATEGY_H
+
+namespace vnn
+{
+  class InputConnector
+  {
+      public:
+          InputConnector() {}
+          ~InputConnector() {}
+  };
+
+
+
+}
+#endif
+
---
+++ src/vnnvideostream.h
@@ -0,0 +1,77 @@
+/**
+ * libvnn
+ * Copyright (c) 2018 Nicolas Bertrand
+ * Author:
+ *
+ * This file is part of libvnn.
+ *
+ * deepdetect is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * deepdetect is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with deepdetect.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef VNNVIDEOSTREAM_H
+#define VNNVIDEOSTREAM_H
+
+namespace vnn
+{
+  template <class TInputConnectorStrategy, class TStreamLib,  class TOutputConnectorStrategy>
+    class VideoStreamNN
+    {
+      public:
+
+        int init()
+        {
+          _input.init()
+          _output.init()
+          _streamlib.init(_input,_output)
+        }
+
+        int run()
+        {
+          _streamlib.run();
+        }
+
+        TInputConnectorStrategy _inputc; /**< input connector strategy for channeling data in. */
+        TOutputConnectorStrategy _outputc; /**< output connector strategy for passing results back to API. */
+        TStreamLib _streamlib;
+
+
+    };
+// TODO: create files for OutputConnector and StreamLib
+#if 0
+  class OutputConnector
+  {
+    int init()
+    {};
+  };
+
+  class StreamLib
+  {
+    int init()
+    {};
+  };
+
+
+  class GstreamerTX2: StreamLib
+  {};
+
+  class GstreamerDesktop: StreamLib
+  {};
+
+  class OutputConnectorBuffer: OutputConnector
+  {};
+#endif
+
+}
+#endif
+
